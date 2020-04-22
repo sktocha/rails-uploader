9e2ef9eba29407f8b4666bae5d92b6aad0ede338
Anton Zlatohorskyi <anton.zlatohorskyi@netfix.xyz>
2020-04-22 10:12:37 +0300

    updates for rails 5

--- app/controllers/uploader/attachments_controller.rb
+++ app/controllers/uploader/attachments_controller.rb
@@ -2,8 +2,8 @@ module Uploader
   class AttachmentsController < ActionController::Metal
     include AbstractController::Callbacks
   
-    before_filter :find_klass
-    before_filter :find_asset, :only => [:destroy]
+    before_action :find_klass
+    before_action :find_asset, :only => [:destroy]
     
     def create
       @asset = @klass.new(params[:asset])
@@ -48,7 +48,7 @@ module Uploader
       
       def render_json(hash_or_object, status = 200)
         self.status = status
-        self.content_type = Uploader.content_type(env["HTTP_USER_AGENT"])
+        self.content_type = Uploader.content_type(request.env["HTTP_USER_AGENT"])
         self.response_body = hash_or_object.to_json(:root => false)
       end
       
